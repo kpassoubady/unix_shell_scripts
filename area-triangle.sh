8720519298059a77e869c9c1d5367b895ca7b3b1
Kangeyan Passoubady <1095193+kpassoubady@users.noreply.github.com>
2022-10-17 00:26:14 -0700

    Updated code

--- area-triangle.sh
+++ area-triangle.sh
@@ -5,6 +5,6 @@ c=$SIDE_C
 
 echo "The side of the triangles: $SIDE_A, $SIDE_B, $SIDE_C"
 
-s=$(echo "scale=4; ($a+$b+$c)/2" | bc)
-area=$(echo "scale=4; sqrt($s*($s-$a)*($s-$b)*($s-$c))" | bc)
+s=`echo "scale=4; ($a+$b+$c)/2" | bc`
+area=`echo "scale=4; sqrt($s*($s-$a)*($s-$b)*($s-$c))" | bc`
 echo "The area of the triangle is $area"
